1594053c44a3d54163e947ef5181dc6c4521de85
Julien Rapacki <105038277+JulienRapacki@users.noreply.github.com>
2024-07-03 14:55:45 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -5,22 +5,12 @@ import requests
 # URL de votre API Azure
 API_URL = "https://p07-api.azurewebsites.net/predict_sentiment"
 
-
-
-
-def get_sentiment(text):
-    response = requests.post(API_URL, json={"text": text})
-    if response.status_code == 200:
-        return response.json()["sentiment"]
-    else:
-        return "Erreur lors de l'appel à l'API"
-
 st.title("Analyse de sentiment")
 
 user_input = st.text_area("Entrez votre phrase ici :")
 
 if st.button("Analyser"):
-    response = requests.post(f"{API_URL}/predict", json={"text": text})
+    response = requests.post(f"{API_URL}/predict", json={"text": user_input })
     prediction = response.json()['sentiment']
     st.write(f"Sentiment prédit : {prediction}")
 

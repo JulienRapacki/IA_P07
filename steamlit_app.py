a9b85ed0dca8c216eed4ad6a6d5c727e6a889d17
Julien Rapacki <105038277+JulienRapacki@users.noreply.github.com>
2024-08-02 14:43:19 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -18,20 +18,20 @@ if st.button("Analyser"):
     st.write(f"Sentiment prédit : {prediction} pour une probabilité de {probability}")
 
 
-if st.button("Prédiction correcte"):
-        
-    requests.post(f"{API_URL}/feedback", params={"prediction": prediction, "is_correct": True})
-    if response.status_code == 200:
-        st.write("Merci pour votre feedback !")
-    else:
-        st.write("Erreur lors de l'envoi du feedback.")
-
-
-if st.button("Prédiction incorrecte"):
-              
-    requests.post(f"{API_URL}/feedback", params={"prediction": prediction, "is_correct": False})
-    if response.status_code == 200:
-        st.write("Merci pour votre feedback !")
-    else:
-        st.write("Erreur lors de l'envoi du feedback.")
+    if st.button("Prédiction correcte"):
+            
+        requests.post(f"{API_URL}/feedback", params={"prediction": prediction, "is_correct": True})
+        if response.status_code == 200:
+            st.write("Merci pour votre feedback !")
+        else:
+            st.write("Erreur lors de l'envoi du feedback.")
+    
+    
+    if st.button("Prédiction incorrecte"):
+                  
+        requests.post(f"{API_URL}/feedback", params={"prediction": prediction, "is_correct": False})
+        if response.status_code == 200:
+            st.write("Merci pour votre feedback !")
+        else:
+            st.write("Erreur lors de l'envoi du feedback.")
 

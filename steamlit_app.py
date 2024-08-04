3e2d00b0f9b3a05cbbb5a51ba155e34ca7c7b948
Julien Rapacki <105038277+JulienRapacki@users.noreply.github.com>
2024-08-04 19:01:37 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
-from opentelemetry import trace
+from opentelemetry.trace import Tracer
+import logging
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
 
@@ -8,13 +9,12 @@ from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
 # URL de votre API Azure
 API_URL = "https://p07.azurewebsites.net"
 
-tracer_provider = trace.get_tracer_provider()
+logger = logging.getLogger(__name__)
+# exporter = AzureMonitorTraceExporter(
+#     connection_string="InstrumentationKey=ec60a799-186d-4345-86af-c5babe81ee62")
+# span_processor = BatchSpanProcessor(exporter)
 
-exporter = AzureMonitorTraceExporter(
-    connection_string="InstrumentationKey=ec60a799-186d-4345-86af-c5babe81ee62")
-span_processor = BatchSpanProcessor(exporter)
-
-tracer = trace.get_tracer(__name__)
+# tracer = trace.get_tracer(__name__)
 
 
 st.title("Analyse de sentiment - Projet 7")
@@ -25,6 +25,7 @@ user_input = st.text_area("Entrez votre phrase ici :")
 
 if st.button("Analyser"):
     with tracer.start_as_current_span("analyze_sentiment") as span:
+        logger.info('analyse ok')
         response = requests.post(f"{API_URL}/predict_sentiment", params={"text":user_input})
         prediction = response.json()['sentiment']
         probability = response.json()['probability']
@@ -42,6 +43,7 @@ if st.button("Analyser"):
         if st.button("Prédiction non conforme"):
             with tracer.start_as_current_span("prediction_feedback") as feedback_span:
                 feedback_span.set_attribute("feedback", "non_conforme")
+                logger.warning('pred not ok")
                 feedback_span.set_attribute("text", user_input)
                 feedback_span.set_attribute("sentiment", prediction)
                 st.error("Merci pour votre retour. Nous allons améliorer notre modèle.")

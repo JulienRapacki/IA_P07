fddac649e380572e60e071f9d6d4a215c7715151
Julien Rapacki <105038277+JulienRapacki@users.noreply.github.com>
2024-08-04 13:17:54 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
 
@@ -9,13 +8,14 @@ from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
 # URL de votre API Azure
 API_URL = "https://p07.azurewebsites.net"
 
-trace.set_tracer_provider(TracerProvider())
-tracer = trace.get_tracer(__name__)
+tracer_provider = trace.get_tracer_provider()
 
 exporter = AzureMonitorTraceExporter(
     connection_string="InstrumentationKey=ec60a799-186d-4345-86af-c5babe81ee62")
 span_processor = BatchSpanProcessor(exporter)
-trace.get_tracer_provider().add_span_processor(span_processor)
+tracer_provider.add_span_processor(span_processor)
+tracer = trace.get_tracer(__name__)
+
 
 st.title("Analyse de sentiment - Projet 7")
 

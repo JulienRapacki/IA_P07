979b36bd7f59cbddd4a5959304667bc20a162562
Julien Rapacki <105038277+JulienRapacki@users.noreply.github.com>
2024-08-05 11:08:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,10 @@ instrumentation_key = "ec60a799-186d-4345-86af-c5babe81ee62"
 configure_azure_monitor(
     connection_string=f"InstrumentationKey={instrumentation_key}")
 
+exporter = AzureMonitorTraceExporter(
+    connection_string=f"InstrumentationKey={instrumentation_key}")
+
+
 # Configuration du tracer
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(exporter))

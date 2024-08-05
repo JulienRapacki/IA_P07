153edd7db2bd9a87390648b395b474987e617043
Julien Rapacki <105038277+JulienRapacki@users.noreply.github.com>
2024-08-05 09:58:52 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -11,11 +11,11 @@ from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
 API_URL = "https://p07.azurewebsites.net"
 
 logger = logging.getLogger(__name__)
-# exporter = AzureMonitorTraceExporter(
-#     connection_string="InstrumentationKey=ec60a799-186d-4345-86af-c5babe81ee62")
-# span_processor = BatchSpanProcessor(exporter)
+exporter = AzureMonitorTraceExporter(
+    connection_string="InstrumentationKey=ec60a799-186d-4345-86af-c5babe81ee62")
+span_processor = BatchSpanProcessor(exporter)
 
-tracer = trace.get_tracer(__name__)
+
 
 
 
@@ -23,7 +23,7 @@ st.title("Analyse de sentiment - Projet 7")
 
 user_input = st.text_area("Entrez votre phrase ici :")
 
-
+tracer = trace.get_tracer(__name__)
 
 if st.button("Analyser"):
     with tracer.start_as_current_span("analyze_sentiment") as span:
@@ -35,17 +35,17 @@ if st.button("Analyser"):
         span.set_attribute("text", user_input)
         span.set_attribute("predicted_sentiment", prediction)
 
-        if st.button("Prédiction conforme"):
-            with tracer.start_as_current_span("prediction_feedback") as feedback_span:
-                feedback_span.set_attribute("feedback", "conforme")
-                feedback_span.set_attribute("text", user_input)
-                feedback_span.set_attribute("sentiment", prediction)
-                st.success("Merci pour votre retour !")
+if st.button("Prédiction conforme"):
+    with tracer.start_as_current_span("prediction_feedback") as feedback_span:
+        feedback_span.set_attribute("feedback", "conforme")
+        feedback_span.set_attribute("text", user_input)
+        feedback_span.set_attribute("sentiment", prediction)
+        st.success("Merci pour votre retour !")
 
-        if st.button("Prédiction non conforme"):
-            with tracer.start_as_current_span("prediction_feedback") as feedback_span:
-                feedback_span.set_attribute("feedback", "non_conforme")
-                logger.warning("pred not ok")
-                feedback_span.set_attribute("text", user_input)
-                feedback_span.set_attribute("sentiment", prediction)
-                st.error("Merci pour votre retour. Nous allons améliorer notre modèle.")
+if st.button("Prédiction non conforme"):
+    with tracer.start_as_current_span("prediction_feedback") as feedback_span:
+        feedback_span.set_attribute("feedback", "non_conforme")
+        logger.warning("pred not ok")
+        feedback_span.set_attribute("text", user_input)
+        feedback_span.set_attribute("sentiment", prediction)
+        st.error("Merci pour votre retour. Nous allons améliorer notre modèle.")
